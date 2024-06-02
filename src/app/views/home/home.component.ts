24d07839bf80753dbfc374da9f442041f591f143
Yohan González Almaguer <yohan.gonzalez.almaguer@gmail.com>
2024-06-01 20:54:56 -0400

    feat: mock-up initial state of home view

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,7 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { TuiButtonModule, TuiHintModule, TuiSvgModule } from '@taiga-ui/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { BaseComponent } from '@rha/common/classes';
+import { forkJoin, interval } from 'rxjs';
+import { ConnectionState, NetworkInfo, NetworkSettings, ProfileData, SimStatus, SystemStatus } from '@rha/common/types';
+import { NETWORKS_TYPES } from '@rha/common';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'rha-home',
@@ -17,5 +22,61 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent extends BaseComponent {
+
+  systemStatus = signal<SystemStatus | null>(null);
+  networkInfo = signal<NetworkInfo | null>(null);
+  networkSettings = signal<NetworkSettings | null>(null);
+  connectionState = signal<ConnectionState | null>(null);
+  simStatus = signal<SimStatus | null>(null);
+  currentProfile = signal<ProfileData | null>(null);
+  buttonLabel = computed(
+    () => this.connectionState()?.ConnectionStatus === 2 ? 'Disconnect' : 'Connect'
+  );
+  internetStatusLabel = computed(
+    () =>
+      this.restarting()
+        ? 'Restarting...'
+        : this.connectionState()?.ConnectionStatus === 2 ? 'Connected' : 'Disconnected'
+  );
+  simStatusLabel = computed(
+    () => this.simStatus()?.PinState === 2 ? 'Ready' : 'Not Ready'
+  );
+  restarting = signal<boolean>(false);
+  networkType = computed(() => NETWORKS_TYPES[ this.systemStatus()!.NetworkType ]);
+
+  constructor() {
+    super();
+    this.#loadHomeData();
+    interval(3 * 1000)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.#loadHomeData());
+  }
+
+  #loadHomeData() {
+    forkJoin({
+      systemStatus: this.linkZone.getSystemStatus(),
+      networkInfo: this.linkZone.getNetworkInfo(),
+      networkSettings: this.linkZone.getNetworkSettings(),
+      connectionState: this.linkZone.getConnectionState(),
+      simStatus: this.linkZone.getSimStatus(),
+      currentProfile: this.linkZone.getCurrentProfile()
+    }).subscribe((res) => {
+      this.systemStatus.set(res.systemStatus.result);
+      this.networkInfo.set(res.networkInfo.result);
+      this.networkSettings.set(res.networkSettings.result);
+      this.connectionState.set(res.connectionState.result);
+      this.simStatus.set(res.simStatus.result);
+      this.currentProfile.set(res.currentProfile.result);
+      this.restarting.set(![ 1, 2 ].includes(this.connectionState()!.ConnectionStatus));
+    });
+  }
+
+  toggleConnection() {
+    (
+      this.connectionState()?.ConnectionStatus === 2
+        ? this.linkZone.disconnectInternet()
+        : this.linkZone.connectInternet()
+    ).subscribe();
+  }
 }
